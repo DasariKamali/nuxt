ad6e79c32f901fe766775b38960bb736e9d824c1
DasariKamali <107987424+DasariKamali@users.noreply.github.com>
2024-08-26 17:11:58 +0530

    Update AuthButton.vue

--- components/AuthButton.vue
+++ components/AuthButton.vue
@@ -6,9 +6,9 @@
   export default {
     methods: {
       redirectToAuth() {
-        const clientId = 'e810c444-49f0-4184-8c26-dc9fa08a5230'; 
-        const redirectUri = 'https://black-sea-07fc4440f.5.azurestaticapps.net/callback'; 
-        const tenantId = '9329c02a-4050-4798-93ae-b6e37b19af6d';
+        const clientId = '<ClientID>'; 
+        const redirectUri = 'https://<AzureStaticWebAppname>.azurestaticapps.net/callback'; 
+        const tenantId = '<TenantID>';
         const authUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`;
         const scope = encodeURIComponent('openid profile');
         const authorizationUrl = `${authUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
